5463783b72e23a220bae01d7d0960e7991e92934
rahimaaa <rakhimakhabib@gmail.com>
2023-09-16 21:36:33 -0400

    created puzzles card component so we can display all the puzzles a player has slved so far

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import PlayerPuzzles from "../components/PlayerPuzzles";
 
 
 function Profile() {
@@ -9,6 +10,21 @@ function Profile() {
       <NavBar />
       <div className = " h-screen">
          <h1>Profile</h1>
+         <div className="flex items-center m-8 mb-20">
+           <div className="w-64 h-64 rounded-full mt-20 ml-12  bg-blue-500"> </div>
+         <div className="m-12 p-8">
+             <h1 >name of the player</h1>
+             <h1>number of puzzles solved</h1>
+              <h1>number of puzzles created</h1>
+              <h1>score</h1>
+              <h1>member since: date</h1>
+         </div>
+        
+         </div>
+         <div>
+          <PlayerPuzzles />
+         </div>
+         
       </div>
       
     </div>
